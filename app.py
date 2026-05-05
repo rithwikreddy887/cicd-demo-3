20bdcb4344237e2f6da548159437c5d70cd3cd96
Your Name <rithwikreddy887@gmail.com>
2026-05-05 02:12:20 +0000

    testing 01

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
 print("Hello CI/CD")
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print(add(2, 3))
---
+++ tests/test_app.py
@@ -0,0 +1,6 @@
+def test_example():
+    assert 1 + 1 == 2
+
+def test_fail():
+    assert 2 * 2 == 4
+

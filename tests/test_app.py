61e62bf0a57de6a6f0721e409bac3738fd309656
Your Name <rithwikreddy887@gmail.com>
2026-05-05 03:09:13 +0000

    updated testing

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,11 @@
-def test_example():
-    assert 1 + 1 == 2
+from app import add
 
-def test_fail():
-    assert 2 * 2 == 
+def test_add_positive():
+    assert add(2, 3) == 5
+
+def test_add_zero():
+    assert add(0, 5) == 5
+
+def test_add_negative():
+    assert add(-2, -3) == -5
 
